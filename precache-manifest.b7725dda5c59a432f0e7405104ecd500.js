6136eb8671c69ba03587696103cf6fa20ea855e9
Venyige Balázs <venyigebalazs96@gmail.com>
2020-07-05 19:58:14 +0200

    Updates

---
+++ precache-manifest.b7725dda5c59a432f0e7405104ecd500.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf8f6b07630fc2114bf0c400ed50ad1",
+    "url": "/lyrics-counter/index.html"
+  },
+  {
+    "revision": "baf8d322286b8753b13b",
+    "url": "/lyrics-counter/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "53133e5e505135abe0a3",
+    "url": "/lyrics-counter/static/css/main.868edb4e.chunk.css"
+  },
+  {
+    "revision": "baf8d322286b8753b13b",
+    "url": "/lyrics-counter/static/js/2.91624614.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/lyrics-counter/static/js/2.91624614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53133e5e505135abe0a3",
+    "url": "/lyrics-counter/static/js/main.4fd59c5e.chunk.js"
+  },
+  {
+    "revision": "27e63c1b5a89f4498767",
+    "url": "/lyrics-counter/static/js/runtime-main.93584974.js"
+  }
+]);
