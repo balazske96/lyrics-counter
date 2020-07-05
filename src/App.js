39476efe66eaa7a6021bd147b0a39cb0d57d1296
Venyige Balázs <venyigebalazs96@gmail.com>
2020-07-05 19:21:27 +0200

    Restart heroku

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import Title from './components/Title/Title';
 import Text from './components/Text/Text';
-import Submit from './components/Submit/Submit';
 import 'antd/dist/antd.css';
 import RepeatedWords from './components/RepeatedWords/RepeatedWords';
 import { AppContextProvider } from './context/AppContext';
